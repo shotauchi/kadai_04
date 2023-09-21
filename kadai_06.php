0d7c89c88834d69b7d17089fed29ad7d0710b8d2
[shotauchi] <[tianneixiang84@gmail.com]>
2023-09-21 02:56:35 +0000

    php05 practice-02

---
+++ kadai_06.php
@@ -0,0 +1,70 @@
+<?php
+// 1〜10 までを足した値を返す関数
+function times($num){
+return $num * 2;
+}  
+
+
+function sumab($a,$b){
+    return $a + $b;
+}
+
+
+function product($arr){
+    $result = $arr[0]; 
+    for ($i = 1; $i < count($arr); $i++) {
+            $result *= $arr[$i];
+    }
+    return $result;
+}
+echo product(array(1, 3, 5 ,7, 9));
+
+
+function max_array($arr){
+  
+  $max_number = $arr[0];
+  foreach($arr as $a){
+    
+  if($max_number < $a){
+         $max_number = $a;
+     }
+  }
+
+  return $max_number;
+} 
+
+echo max_array(array(0));
+
+
+//strip_tags
+  $str = "<h1> apple </h1>"
+  . "<p> orange </p>";
+echo strip_tags($str) ."¥n";
+
+
+//array_push
+$stack = array("hamburger", "steak");
+array_push($stack, "rice ball", "bread");
+print_r($stack);
+  
+
+//array_merge
+$array1 = array("color" => "blue", 1, 3);
+$array2 = array("a", "b", "color" => "white", "shape" => "square", 3);
+$result = array_merge($array1, $array2);
+print_r($result);
+
+
+//time
+echo 'Now: '. time();
+
+
+//mktime
+echo date("M-d-Y", mktime(0, 0, 0, 12, 30, 2022));
+echo date("M-d-Y", mktime(0, 0, 0, 12, 31, 2022));
+echo date("M-d-Y", mktime(0, 0, 0, 1, 1, 2023));
+echo date("M-d-Y", mktime(0, 0, 0, 1, 1, 23));
+
+//date
+$now = time();
+print $now;
